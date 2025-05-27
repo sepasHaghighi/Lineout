d860e7f48854948fc948a183c85f95d4e31ca73f
Sepas <haghighi.sepas@gmail.com>
2025-05-27 16:35:32 +0330

    All the pages are now implemented.

--- src/Pages/Navigation.tsx
+++ src/Pages/Navigation.tsx
@@ -7,8 +7,9 @@ export default function Navigation() {
         <div className="flex flex-col items-center">
             <Header menu={true} back={false} />
             <section className="flex flex-col items-center gap-4 justify-center w-[375px] p-4">
+                <h1>Direction</h1>
                 <img src={locationImage} alt="" className="w-[100vw] rounded-xl" />
-                <section className="h-[50vh] flex flex-col justify-end items-center gap-4">
+                <section className="h-[40vh] flex flex-col justify-end items-center gap-4">
                     <Button link={`#`} text="I arrived" disabled={false} />
                     <Button link={`#`} text="I need more time" disabled={false} />
                     <a href="/searchBy">Cancel</a>
